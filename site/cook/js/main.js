47f75d8fdf8bc135395d728ed8b1f10ebb4e1ea0
fox-of-red <101697714+fox-of-red@users.noreply.github.com>
2022-08-15 11:10:17 +0900

    Add files via upload

---
+++ site/cook/js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+ {
+   function callback(entries, obs){
+     entries.forEach(entry => {
+       if(entry.isIntersecting === true) {
+         entry.target.classList.add('appear');
+         obs.unobserve(entry.target);
+       }
+     });
+   }
+   const options = {
+     threshold: .5,
+   }
+   const observer = new IntersectionObserver(callback, options)
+   document.querySelectorAll('#subvisial > li').forEach(el => {
+     observer.observe(el);
+     
+   })
+ }
